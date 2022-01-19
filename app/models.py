b2be8bbc67d41d771137aa2f84ce366231e89ccb
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-01-19 08:46:15 +0300

    templates testing

--- app/models.py
+++ app/models.py
@@ -3,9 +3,50 @@ from app import app
 
 # Views
 @app.route('/')
-def base():
+def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('about.html')
+
+# categories
+@app.route('/women')
+def  women():
+    return render_template('women.html')
+
+
+@app.route('/men')
+def men():
+    return render_template('men.html')
+
+@app.route('/boys')
+def boys():
+    return render_template('boys.html')
+
+
+@app.route('/girls')
+def girls():
+    return render_template('girls.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+
+
+
+
+
+
+
